af0d9e9a515e6f39ed70ef7b777f4ba0bedbcfbe
Derek Xia <kaized@10-17-40-151.dynapool.wireless.nyu.edu>
2024-11-08 17:30:39 -0500

    imported css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Logo from './ui/Logo';  // Your existing Logo component
 import Icon from './ui/Icon';  // The new Icon component
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="IconContainer">
-        <Icon height={50} width={50} />
-      </div>
-      <div className="LogoContainer">
-        <Logo height={100} width={100} />  {/* Your Logo component */}
+    <div className = "App">
+      <div className="header">
+        <div className="IconContainer">
+          <Icon height={50} width={50} />
+        </div>
+        <div className="LogoContainer">
+          <Logo height={100} width={100} />  {/* Your Logo component */}
+        </div>
       </div>
       <h1>Welcome to LangCon</h1>
     </div>
