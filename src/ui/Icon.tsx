477733e215d82838eca93c471d747fb0d583354f
Derek Xia <dx2119@nyu.edu>
2024-11-15 21:53:05 -0500

    tried to make the icon a button so I could add a dropdown feature after

--- src/ui/Icon.tsx
+++ src/ui/Icon.tsx
@@ -3,8 +3,26 @@ import LogoIcon from '../assets/images/logo.svg';
 type LogoProps = {
   height: number;
   width: number;
+  onClick?: () => void;
 };
 
 export default function Icon(props: LogoProps) {
-  return <img src={LogoIcon} height={props.height} width={props.width} alt="LangConv Logo" />;
+  return (
+    <button
+      onClick={props.onClick}
+      style={{
+        border: 'none',
+        background: 'none',
+        padding: 0,
+        cursor: 'pointer',
+      }}
+    >
+      <img
+        src={LogoIcon}
+        height={props.height}
+        width={props.width}
+        alt="LangConv Logo"
+      />
+    </button>
+  );
 }
